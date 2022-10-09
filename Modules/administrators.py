219c011d8fd7850d9730f739d557464b334d2dd9
Earl <zonedvoliomu@gmail.com>
2022-10-09 22:39:34 +0600

    Update administrators.py

--- Modules/administrators.py
+++ Modules/administrators.py
@@ -62,18 +62,6 @@ async def delete_clan(ans: Message, id):
     delete(clans, int(id))
     return f"&#128737; @id{ans.from_id}({user['nick']}), вы успешно удалили клан {clan['name']}[ID: {id}]!"
 
-@bp.on.message(text="изменить <json> <ids> <id> <name> <num>")
-async def dump_json(ans: Message, json, ids, id, name, num):
-    user = find("id", ans.from_id, users)
-    if user['rights'] < 3:
-        return f"&#128219; @id{ans.from_id}({user['nick']}), у вас недостаточно прав это использовать!"
-    jsons = find(ids, int(id), f"Other/{json}.json")
-    if not jsons:
-        return f"&#128219; @id{ans.from_id}({user['nick']}), такого {id} id в Other/{json}.json отсутсвует!"
-    jsons[name] = int(num)
-    dumpjson(jsons, f"Other/{json}.json")
-    return f" @id{ans.from_id}({user['nick']}), вы изменили в Other/{json}.json файле пункты: \n {name} : {num}"
-
 @bp.on.message(text="отправить <id> <texts>")
 async def message_to_persone(ans: Message, id, texts):
     user = find("id", ans.from_id, users)
@@ -87,6 +75,59 @@ async def message_to_persone(ans: Message, id, texts):
         return f"&#128213; @id{user['id']}({user['nick']}), вы отправили пользователю @id{userss['id']}({userss['nick']}) сообщение:\n&#128195; {texts}"
     except Exception:
         return f"&#128219; @id{ans.from_id}({user['nick']}), у пользователя @id{userss['id']}({userss['nick']}) закрыты личные сообщения!"
+
+@bp.on.nessage(text="все пользователи")
+async def all_users(ans:Message):
+    user = find("id", ans.from_id, users)
+    if user['rights'] < 2:
+        return f"&#128219; @id{ans.from_id}({user['nick']}), у вас недостаточно прав это использовать!"
+    num = 0
+    for x in loadjson(users):
+        num += 1
+    return f" @id{ans.from_id}({user['nick']}), всего пользователей: {num}"
+
+@bp.on.nessage(text="список пользователей")
+async def all_users(ans:Message):
+    user = find("id", ans.from_id, users)
+    if user['rights'] < 2:
+        return f"&#128219; @id{ans.from_id}({user['nick']}), у вас недостаточно прав это использовать!"
+    text = ""
+    for i in loadjson(users):
+        text += f"@id{i['id']}({i['nick']})\n"
+    await ans.answer(f"&#128424; @id{ans.from_id}({user['nick']}), список пользователей:\n" + text)
+
+@bp.on.nessage(text="все кланы")
+async def all_users(ans:Message):
+    user = find("id", ans.from_id, users)
+    if user['rights'] < 2:
+        return f"&#128219; @id{ans.from_id}({user['nick']}), у вас недостаточно прав это использовать!"
+    num = 0
+    for x in loadjson(clans):
+        num += 1
+    return f" @id{ans.from_id}({user['nick']}), всего кланов: {num}"
+
+@bp.on.nessage(text="список кланов")
+async def all_users(ans:Message):
+    user = find("id", ans.from_id, users)
+    if user['rights'] < 2:
+        return f"&#128219; @id{ans.from_id}({user['nick']}), у вас недостаточно прав это использовать!"
+    text = ""
+    for i in loadjson(clans):
+        text += f"@id{i['owner']}({i['name']})\n"
+    await ans.answer(f"&#128424; @id{ans.from_id}({user['nick']}), список кланов:\n" + text)
+
+
+@bp.on.message(text="изменить <json> <ids> <id> <name> <num>")
+async def dump_json(ans: Message, json, ids, id, name, num):
+    user = find("id", ans.from_id, users)
+    if user['rights'] < 3:
+        return f"&#128219; @id{ans.from_id}({user['nick']}), у вас недостаточно прав это использовать!"
+    jsons = find(ids, int(id), f"Other/{json}.json")
+    if not jsons:
+        return f"&#128219; @id{ans.from_id}({user['nick']}), такого {id} id в Other/{json}.json отсутсвует!"
+    jsons[name] = int(num)
+    dumpjson(jsons, f"Other/{json}.json")
+    return f" @id{ans.from_id}({user['nick']}), вы изменили в Other/{json}.json файле пункты: \n {name} : {num}"
 
 @bp.on.message(text="jsons")
 async def log_users(ans: Message):
